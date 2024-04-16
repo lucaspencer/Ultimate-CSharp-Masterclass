71af4f6d31bd3685096d3e7a2ca070edd46e910f
lucaspencer <lucaspencer0@gmail.com>
2024-04-16 17:00:24 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,48 @@
+﻿
+int GetNumber()
+{
+    int num;
+    while (!int.TryParse(Console.ReadLine(), out num))
+    {
+        Console.WriteLine("Invalid input. Please try again.\n");
+    }
+    return num;
+}
+
+void GetOperation(int num1, int num2)
+{
+    bool _ = true;
+    char input;
+    while (_)
+    {
+        input = char.ToUpper(Console.ReadKey().KeyChar);
+        
+        switch (input)
+        {
+            case 'A':
+                Console.WriteLine($"\n{num1} + {num2} = {num1 + num2}");
+                _ = false;
+                break;
+            case 'S':
+                Console.WriteLine($"\n{num1} - {num2} = {num1 - num2}");
+                _ = false;
+                break;
+            case 'M':
+                Console.WriteLine($"\n{num1} ÷ {num2} = {num1 / num2}");
+                _ = false;
+                break;
+            default:
+                Console.WriteLine("\nInvalid input. Please try again.\n");
+                break;   
+        }
+    }
+}
+
+Console.WriteLine("Hello!\nInput the first number:");
+int num1 = GetNumber();
+Console.WriteLine("Input the second number:");
+int num2 = GetNumber();
+Console.WriteLine("What do you want to do with those numbers?\n[A]dd\n[S]ubtract\n[M]ultiply");
+GetOperation(num1, num2);
+Console.WriteLine("Press any key to close");
+Console.ReadLine();
---
+++ TodoList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Channels;
+
+char input;
+bool running = true;
+
+void PrintSelected(string option)
+{
+    Console.WriteLine("Selected option: " + option);
+}
+void SeeTodo()
+{
+    PrintSelected("See all TODOs");
+}
+
+void AddTodo()
+{
+    PrintSelected("Add a TODO");
+}
+
+void RemoveTodo()
+{
+    PrintSelected("Remove a TODO");
+}
+
+while (running)
+{
+    Console.WriteLine("Hello!\nWhat do you want to do?\n[S]ee all TODOs\n[A]dd a TODO\n[R]emove a todo\n[E]xit");
+    input = char.ToUpper(Console.ReadKey().KeyChar);
+    switch (input)
+    {
+        case 'S':
+            SeeTodo();
+            break;
+        case 'A':
+            AddTodo();
+            break;
+        case 'R':
+            RemoveTodo();
+            break;
+        case 'E':
+            running = false;
+            break;
+        default:
+            Console.WriteLine(input + "is an invalid input.");
+            break;
+    }
+}
+Console.WriteLine("TODO list closed. Have a nice day.\nPress any key to exit.");
+Console.ReadKey();
